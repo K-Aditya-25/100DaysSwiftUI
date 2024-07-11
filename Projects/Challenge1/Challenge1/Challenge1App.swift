fed9b439a3081b43085700dbef5da2002a6ff138
Aditya Kharbanda <adityakharbanda25@gmail.com>
2024-07-11 11:38:23 +0530

    Started Challenge 1

---
+++ Projects/Challenge1/Challenge1/Challenge1App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge1App.swift
+//  Challenge1
+//
+//  Created by Aditya Kharbanda on 11/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Challenge1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
