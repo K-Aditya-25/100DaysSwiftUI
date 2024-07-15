97e6d6e3d5771a1952ad21986b69f0fae23950ff
Aditya Kharbanda <adityakharbanda25@gmail.com>
2024-07-16 00:19:46 +0530

    Added View Modifier and extension to View

--- Projects/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ Projects/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -7,6 +7,23 @@
 
 import SwiftUI
 
+//Challenge: make a view modifier with the following specifications
+struct ProminentFont: ViewModifier{
+    func body(content: Content) -> some View {
+        content
+            .fontWeight(.bold)
+            .font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
+            .foregroundStyle(.blue)
+    }
+}
+
+//Make a corresponding extension to View
+extension View{
+    func prominentFont() -> some View{
+        modifier(ProminentFont())
+    }
+}
+
 struct ContentView: View {
     var body: some View {
         VStack {
@@ -14,6 +31,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+                .prominentFont()
         }
         .padding()
     }
