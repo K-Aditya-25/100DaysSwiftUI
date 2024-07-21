14aee2ec4c51a10df4499f85a72cc31f29d83e60
Aditya Kharbanda <adityakharbanda25@gmail.com>
2024-07-21 16:17:15 +0530

    BetterRest App works

--- Projects/BetterRest/BetterRest/ContentView.swift
+++ Projects/BetterRest/BetterRest/ContentView.swift
@@ -5,39 +5,77 @@
 //  Created by Aditya Kharbanda on 20/07/24.
 //
 
+import CoreML
 import SwiftUI
 
 struct ContentView: View {
+    @State private var wakeUpTime = defaultWakeTime
     @State private var sleepAmount = 8.0
-    @State private var wakeUp = Date.now
+    @State private var coffeeAmount = 1
+    
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
+    
+    static var defaultWakeTime: Date{
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? .now
+    }
     var body: some View {
-        Stepper("\(sleepAmount.formatted(.number)) hours of sleep", value: $sleepAmount, in: 4...12, step:0.25)
-            .padding(20)
-        
-        DatePicker("Please select date: ", selection: $wakeUp, in: Date.now... , displayedComponents: .hourAndMinute)
-            .labelsHidden()
-            .padding(20)
-        Text(Date.now, format: .dateTime.day().month().year())
-        Text(Date.now.formatted(date: .long, time: .shortened))
+        NavigationStack{
+            Form{
+                VStack(alignment: .leading ,spacing:0){
+                    Text("What Time Do You Wake Up?")
+                        .font(.headline)
+                        .padding(.bottom)
+                    DatePicker("Please Enter A Time", selection: $wakeUpTime, displayedComponents: .hourAndMinute)
+                        .labelsHidden()
+                }
+                VStack(alignment: .leading ,spacing:0){
+                    Text("Desired Amount Of Sleep?")
+                        .font(.headline)
+                    Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 2...12, step: 0.25)
+                }
+                VStack(alignment: .leading ,spacing:0){
+                    Text("Daily Coffee Intake")
+                        .font(.headline)
+                    Stepper("^[\(coffeeAmount) cup](inflect: true)", value: $coffeeAmount, in: 0...20)
+                }
+            }
+            .navigationTitle("Better Rest")
+            .toolbar{
+                Button("Calculate", action: calculateSleepTime)
+            }
+            .alert(alertTitle, isPresented: $showingAlert){
+                Button("OK"){}
+            }message: {
+                Text(alertMessage)
+            }
+        }
     }
     
-    func exampleDates(){
-//        let now = Date.now
-//        let tomorrow = Date.now.addingTimeInterval(86400) //no. of seconds in a day = 86400
-//        let range = now...tomorrow
-        
-//        var components = DateComponents()
-//        components.hour = 6
-//        components.minute = 30
-//        let date = Calendar.current.date(from: components) ?? .now
-        
-        let components = Calendar.current.dateComponents([.hour, .minute], from: .now)
-        let hour = components.hour ?? 0
-        let minute = components.minute ?? 0
-        
-        
-        
+    func calculateSleepTime(){
+        do{
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUpTime)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            let sleepTime = wakeUpTime - prediction.actualSleep
+            
+            alertTitle = "Your Ideal Bedtime Is..."
+            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+        }catch{
+            alertTitle = "Error!"
+            alertMessage = "Sorry, prediction failed."
+        }
+        showingAlert = true
     }
+    
 }
 
 #Preview {
