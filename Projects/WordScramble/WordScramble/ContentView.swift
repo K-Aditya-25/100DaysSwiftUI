3e144cb148bc86158228ea80298f8ef309a12451
Aditya Kharbanda <adityakharbanda25@gmail.com>
2024-07-25 00:31:55 +0530

    Added code related to manipulating Strings

--- Projects/WordScramble/WordScramble/ContentView.swift
+++ Projects/WordScramble/WordScramble/ContentView.swift
@@ -8,18 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
+    let names = "Aditya Siddharth Anu Bharat"
+    let letters = ["a", "b", "c"]
+    let animal = "    cat       "
+    let testWord = "best"
     var body: some View {
         
-        /// Example of some Bundle Code
-        var body: some View {
-            if let fileURL = Bundle.main.url(forResource: "some-file", withExtension: "txt"){
-                //we found the file in our bundle
+        List{
+            Section("Separate Strings"){
+                ForEach(separateStrings(names), id: \.self){
+                    Text("\($0)")
+                }
+            }
+            Section("Random String"){
+                Text(randomString(from: letters))
+            }
+            Section("Remove White Space"){
+                Text(trimWhiteSpace(in: animal))
             }
             
-            if let fileContents = try? String(contentsOf: fileURL){
-                //we loaded the file into a String
+            Section("Test Spelling"){
+                Text(String(spellCheck(testWord)))
             }
         }
+        
+    }
+    func separateStrings(_ string: String) -> [String]{
+       string.components(separatedBy: " ")
+    }
+    
+    func randomString(from array: [String]) -> String{
+        array.randomElement() ?? "Error"//This gives an optional value
+    }
+    
+    func trimWhiteSpace(in string: String) -> String{
+        string.trimmingCharacters(in: .whitespacesAndNewlines) //check for more options by only typing the dot
+    }
+    
+    func spellCheck(_ string: String) -> Bool{
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: testWord.utf16.count)
+        let missplledRange = checker.rangeOfMisspelledWord(in: testWord, range: range, startingAt: 0, wrap: false, language: "en") //Objective C did not have a concept of Optional. If no spelling mistake is found, it will return NSNotFound
+        
+        //Address NSNotFound
+        let allGood = missplledRange.location == NSNotFound
+        return allGood
     }
 }
 #Preview {
