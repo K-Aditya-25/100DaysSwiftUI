90a1f6a40fb64637dc5c7df4371ccc090e4973de
Aditya Kharbanda <adityakharbanda25@gmail.com>
2024-07-25 02:46:35 +0530

    Complleted the WordScramble App

--- Projects/WordScramble/WordScramble/ContentView.swift
+++ Projects/WordScramble/WordScramble/ContentView.swift
@@ -11,11 +11,16 @@ struct ContentView: View {
     @State private var rootWord = ""
     @State private var listOfWords = [String]()
     @State private var newWord = ""
+    
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
     var body: some View {
         NavigationStack{
             List{
                 Section{
                     TextField("Enter Your Word", text: $newWord)
+                        .autocorrectionDisabled()
                         .textInputAutocapitalization(.never)
                 }
                 Section{
@@ -30,9 +35,12 @@ struct ContentView: View {
             }
             .navigationTitle(rootWord)
             .onSubmit(addWordToList) //onSubmit takes a function that has no parameters and returns nothing
-            .onAppear(perform: {
+            .onAppear(perform: { //runs the code in its scope just when the code compiles and app starts running
                 startGame()
             })
+            .alert(errorTitle, isPresented: $showingError){}message: {
+                Text(errorMessage)
+            }
             
         }
     }
@@ -41,6 +49,22 @@ struct ContentView: View {
         
         //exit function if new word length is 0
         guard  word.count > 0 else{return}
+        
+        //validate input
+        guard isOriginal(word: newWord) else {
+            wordError(title: "Word used already", message: "Be more original")
+            return
+        }
+
+        guard isPossible(word: newWord) else {
+            wordError(title: "Word not possible", message: "You can't spell that word from '\(rootWord)'!")
+            return
+        }
+
+        guard isReal(word: newWord) else {
+            wordError(title: "Word not recognized", message: "You can't just make them up, you know!")
+            return
+        }
         
         //insert word into list
         withAnimation{
@@ -62,6 +86,37 @@ struct ContentView: View {
             }
         }
         fatalError("Damn, couldn't load the text file for some reason!")
+    }
+    
+    func isOriginal(word: String) -> Bool{
+        !listOfWords.contains(word)
+    }
+    
+    func isPossible(word: String)->Bool{
+        var tempWord = rootWord
+        
+        for letter in word{
+            if let pos = tempWord.firstIndex(of: letter){
+                tempWord.remove(at: pos)
+            }else{
+                return false
+            }
+        }
+        return true
+    }
+    
+    func isReal(word: String) -> Bool{
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        return misspelledRange.location == NSNotFound
+    }
+    
+    func wordError(title: String, message: String){
+        errorMessage = message
+        errorTitle = title
+        showingError = true
     }
 }
 #Preview {
